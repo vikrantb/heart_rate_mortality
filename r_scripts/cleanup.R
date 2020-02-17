bd142661a13140fa8e17a9bcb1e2c2b37008ce40
Vikrant Bhosale <vikrantbhosale@hotmail.com>
2020-02-17 00:32:12 -0800

    adding filtering strategy

--- r_scripts/cleanup.R
+++ r_scripts/cleanup.R
@@ -1,1 +1,6 @@
 # test file
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+print (getwd())
+hr <- read.csv(file = '../csvs/heartrate.csv')
+write.csv(subset(hr, Data_Value_Footnote_Symbol != "~"), '../csvs/filtered_heartrate.csv')
